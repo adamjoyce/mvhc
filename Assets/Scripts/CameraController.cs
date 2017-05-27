c019c7c6e346ac04438dbc8894dcf72b42e04429
Adam Joyce <adamjoyce@outlook.com>
2017-05-27 21:11:16 +0100

    Basic player click movement setup

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;           // For player character transform information.
+    private Vector3 offset;             // Distance between the player and the camera.
+
+    // Use this for initialization.
+    void Start()
+    {
+        if (!player)
+        {
+            player = GameObject.Find("Player");
+        }
+
+        offset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame.
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //private Animator anim;                          // Animator for idle and moving animations.
+    private NavMeshAgent navMeshAgent;              // Pathfinding component for click movement.
+    private Transform targetedEnemy;                // The enemy that is being clicked on.
+    private bool walking;                           // Play walk animation when true.
+    private bool enemyClicked;                      // Move towards clicked enemy.
+    private int raycastDistance = 100;              // Maximum distance from the player camera to any terrain.
+
+    // Use this for initialization.
+    void Awake()
+    {
+        //anim = GetComponent<Animator>();
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame.
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Input.GetButtonDown("Fire2"))
+        {
+            if (Physics.Raycast(ray, out hit, raycastDistance))
+            {
+                if (hit.collider.CompareTag("Enemy"))
+                {
+                    // Enemy clicked.
+                    targetedEnemy = hit.transform;
+                    enemyClicked = true;
+                }
+                else
+                {
+                    // Ground clicked.
+                    walking = true;
+                    enemyClicked = false;
+                    navMeshAgent.destination = hit.point;
+                    navMeshAgent.Resume();
+                }
+            }
+        }
+    }
+}
